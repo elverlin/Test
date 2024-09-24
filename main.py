165daf567bcb69b5b737be2def7d399f14d69c95
elverlin <abdoul.karim.elverlin@gmail.com>
2024-09-24 02:17:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import flet as ft
-import requests
-import time
-import re
-import os
+import yt_dlp
+
 
 
 def main(page: ft.Page):
     
-    def get_seasonal_anime(e):
+    def my_hook(d):
+        if d['status'] == 'downloading':
+            infos.value = f"Téléchargement : {d['_percent_str']} complété à une vitesse de {d['_speed_str']}, temps restant : {d['_eta_str']}"
+            infos.update()
+            
+        elif d['status'] == 'finished':
+            infos.value = 'Téléchargement terminé, démarrage du post-traitement...'
+            infos.update()
+
+    def telecharger_video(e):
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': '/storage/emulated/0/Download/%(title)s.%(ext)s',
+            'progress_hooks': [my_hook],
+        }
+        
         try:
-            response = requests.get("https://api.jikan.moe/v4/seasons/now")
-            if response.status_code == 200:
-                page.add(ft.Text("Good request"))
-            
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([f"https://video.sibnet.ru/shell.php?videoid={video_id.value}.mp4"])
+                
         except:
-            page.add(ft.Text("Bad request"))
+            infos.value = 'Erreur de téléchargement'
+            infos.update()
             
-    def test(e):
-        page.add(ft.Text("Je suis un texte"))   
-        page.update()     
     
-    bouton = ft.Row([ft.ElevatedButton("Bouton", on_click=get_seasonal_anime)], alignment=ft.MainAxisAlignment.CENTER)
-    bouton2 = ft.Row([ft.ElevatedButton("Bouton", on_click=test)], alignment=ft.MainAxisAlignment.CENTER)
+    infos = ft.Text(value="")
+    bouton = ft.ElevatedButton(text="Download", on_click=telecharger_video)
+    video_id = ft.TextField()
     
-    
-    page.add(bouton, bouton2)
-    
+    page.add(ft.Row([video_id, bouton], alignment=ft.MainAxisAlignment.CENTER), infos)
+
 ft.app(target=main)
